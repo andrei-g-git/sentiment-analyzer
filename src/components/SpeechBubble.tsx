367060e2369608222b6fb0147c26e707742d6ed0
unknown <andrei_g_git@protonmail.com>
2022-11-06 13:07:16 +0200

    anim delay fix

--- src/components/SpeechBubble.tsx
+++ src/components/SpeechBubble.tsx
@@ -1,13 +1,25 @@
+import {useState} from "react";
 import "../css/SpeechBubble.scss";
 
 function SpeechBubble(props: any) {
+
+    const [isPaused, setPaused] = useState(false);
+
     return (
         <div className={`bubble-container 
-                corner-bottom-left            
-                bottom-left-${props.top}             
-                slide-left`
+                corner-${props.position}            
+                ${props.position}-${props.top}             
+                slide-${props.slide}`
             }
-            style={{top: `${props.top}%`}}
+            style={{
+                top: `${props.top}%`,
+                animation: `slide-${props.slide} ${props.animationDuration}s linear infinite`,
+                animationDelay: `${props.delay}s`,
+                animationFillMode: props.fillMode,
+                animationPlayState: isPaused ? "paused" : "running"
+            }}
+            onMouseOver={() => setPaused(true)}
+            onMouseLeave={() => setPaused(false)}
         >
             {props.text}
         </div>
--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -6,8 +6,33 @@ function Top() {
         <div className="top">
             <SpeechBubble 
                 text="waef fweafawef awefawefawe fawefawef  waef eg 4ygret re!!!!" 
-                top={30}
+                position="top-left"
+                slide="right"
+                animationDuration={7}
+                delay={1}
+                fillMode="backwards" 
+                top={10}
             />
+
+            <SpeechBubble 
+                text="aaaaa a aaaaa aaaaaaa" 
+                position="bottom-right"
+                slide="left"
+                animationDuration={15}
+                delay={2}
+                fillMode="backwards" 
+                top={30}
+            />    
+
+            <SpeechBubble 
+                text="gggggg g gggggggg g gggggg wetewf wefew ggggggggggwef wef fwewewewewewe" 
+                position="top-left"
+                slide="right"
+                animationDuration={10}
+                delay={3}
+                fillMode="backwards" 
+                top={20}
+            />                    
         </div>
     );
 };
