a9766d4e4ffbd44e26282bbed6a2499cc2fd9d6b
unknown <andrei_g_git@protonmail.com>
2022-11-08 16:25:00 +0200

    add social media links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,15 @@
 import Contact from './Contact';
+import Social from './Social';
 import arrowsUp from "../../assets/img/arrows-up.png";
 import "./Footer.scss";
 import "../../css/Headings.scss";
+
 
 function Footer() {
     return (
         <div className="footer">
             <Contact />
+            <Social />
             <img className="back-to-top"
                 src={arrowsUp}
                 alt="arrow"
@@ -16,7 +19,6 @@ function Footer() {
                 Back to top
             </div>         
         </div>
-
     );
 };
 
---
+++ src/components/Footer/Social.tsx
@@ -0,0 +1,27 @@
+import github from "../../assets/img/github.png";
+import twitter from "../../assets/img/twitter.png";
+//import backslash from "../../assets/img/backslash.png";
+import "./Social.scss";
+
+function Social() {
+    return (
+        <div className="social-container">
+            <a href="https://github.com/andrei-g-git">
+                <img className="social-media-icon"
+                    src={github}
+                    alt="git"
+                />                
+            </a>
+            <div className="backslash"/>
+            <a href="">
+                <img className="social-media-icon"
+                    src={twitter}
+                    alt="twitter"
+                />                
+            </a>
+        </div>
+    );
+};
+
+
+export default Social;
