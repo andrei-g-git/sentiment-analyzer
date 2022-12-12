091d6a84b4688d620ad7377fd92d474270b77095
unknown <andrei_g_git@protonmail.com>
2022-12-12 12:55:33 +0200

    add wave animation to mic, buggy

--- src/components/form/Mic.tsx
+++ src/components/form/Mic.tsx
@@ -7,20 +7,27 @@ import "./Mic.scss";
 
 export const Mic = (props: any) => {
     return (
-        <a className={getMicClass(props.isRecording)} //it's a hyperlink so I can use the :target pseudo class
+        <div className={getMicClass(props.isRecording)} //it's a hyperlink so I can use the :target pseudo class
             onClick={() => props.toggleMic(!props.isRecording)}
+            tabIndex={-1} //for click, :active is for mouse-down...
         >
-
-        </a>
+            <div className={getWaveClass(props.isRecording)}></div>
+        </div>
     );
 };
 
 const getMicClass = (isRecording: boolean): string => { //no idea why ternary expressions don't work...
-    const baseClass = "microphone";
-    let extraClass = " black-mic";
-    if(isRecording) extraClass = " red-mic"
-    return baseClass + extraClass;
+    // const baseClass = "microphone";
+    // let extraClass = " black-mic";
+    // if(isRecording) extraClass = " red-mic"
+    // return baseClass + extraClass;
+    if(isRecording) return "red-mic";
+    return "black-mic";
 };
+const getWaveClass = (isRecording: boolean): string => {
+    if(isRecording) return "mic-wave";
+    return ""; 
+}
 
 const mapStateToProps = (state: any) => {
     return {
